750a58a99e0ddda678837ef49e993508c696e616
Ilya Dudelzak <DIA070611@gmail.com>
2026-06-01 09:51:13 +0300

    Added Spring Security login and a root page

---
+++ src/main/java/org/example/goit_java_dev_hw_14/controller/RootController.java
@@ -0,0 +1,16 @@
+package org.example.goit_java_dev_hw_14.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+
+
+    @GetMapping
+    public String root() {
+        return "index";
+    }
+}
